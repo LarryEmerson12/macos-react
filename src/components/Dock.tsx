1e5c6fcc44e44edd6398f0269073964c6f18a5cd
LarryEmerson12 <158800143+LarryEmerson12@users.noreply.github.com>
2025-01-10 17:19:51 +0800

    Add tranlucent effect to dock and add cursor image

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,6 +1,6 @@
 export default function Dock() {
   return (
-    <div className="w-[40ch] h-[7ch] bg-gray-800 rounded-2xl flex justify-around items-center">
+    <div className="w-[40ch] h-[7ch] bg-black/10 rounded-2xl flex justify-around items-center backdrop-blur-lg">
       {[...Array(6)].map((_, i) => (
         <div key={i} className="bg-gray-500 rounded-xl w-[5ch] h-[5ch]" />
       ))}
