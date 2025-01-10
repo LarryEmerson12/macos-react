9f78532ae2fd306181fd09865be143c2c1a37bf7
LarryEmerson12 <158800143+LarryEmerson12@users.noreply.github.com>
2025-01-10 16:55:42 +0800

    Add wallpaper to home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 "use client"
 
-export default function Page() {
-  return <div></div>;
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-[url('/big-sur.jpg')] bg-cover bg-center p-4">
+      <h1 className="text-3xl font-semibold">Macos in React</h1>
+    </div>
+  );
 }
