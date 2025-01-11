11c820bd70ebf7df653f6e8e33b007cf62681835
LarryEmerson12 <158800143+LarryEmerson12@users.noreply.github.com>
2025-01-11 16:37:47 +0800

    Make cursor hide in mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-[url('/big-sur.jpg')] bg-cover bg-center p-4 flex justify-center items-end">
       <Dock />
-      <div className="fixed top-2 left-2">
+      <div className="hidden lg:block fixed top-2 left-2">
         <CursorHandler />
       </div>
     </div>
