a5e9fd451147ace58cd4d173245bc971feeecf2d
LarryEmerson12 <158800143+LarryEmerson12@users.noreply.github.com>
2025-01-11 16:13:23 +0800

    Make cursor go up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import Dock from "@/components/Dock";
 export default function Home() {
   return (
     <div className="min-h-screen bg-[url('/big-sur.jpg')] bg-cover bg-center p-4 flex justify-center items-end">
+      <Dock />
       <div className="fixed top-2 left-2">
         <CursorHandler />
       </div>
-      <Dock />
     </div>
   );
 }
