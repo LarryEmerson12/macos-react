3c8bf0cae7f736c73dd2c695c7911915dbfb5264
LarryEmerson12 <158800143+LarryEmerson12@users.noreply.github.com>
2025-01-18 11:01:59 +0800

    Remove iPad cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import CursorHandler from "@/components/CursorHandler";
 import Dock from "@/components/Dock";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[url('/big-sur.jpg')] bg-cover bg-center p-4 flex justify-center items-end">
       <Dock />
-      <div className="hidden lg:block fixed top-2 left-2">
-        <CursorHandler />
-      </div>
     </div>
   );
 }
