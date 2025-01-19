72e24e4d80299a27822bbb7a62b514e4a6c24168
LarryEmerson12 <158800143+LarryEmerson12@users.noreply.github.com>
2025-01-19 12:39:57 +0800

    Fix context menu error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,39 @@
 
 import React from "react";
 import Dock from "@/components/Dock";
+import { useState } from "react";
+import ContextMenu from "@/components/ContextMenu";
+
+const initialContextMenu = {
+  show: false,
+  x: 0,
+  y: 0,
+};
 
 export default function Home() {
+  const [contextMenu, setContextMenu] = useState(initialContextMenu);
+  const contextMenuClose = () => setContextMenu(initialContextMenu);
+
+  const handleContextMenu = (
+    e: React.MouseEvent<HTMLElement, globalThis.MouseEvent>
+  ) => {
+    console.log("Context menu triggered", e.pageX, e.pageY); // Debugging
+    e.preventDefault();
+    const { pageX, pageY } = e;
+    setContextMenu({ show: true, x: pageX, y: pageY });
+  };
   return (
     <div
+      onContextMenu={handleContextMenu}
       className="min-h-screen bg-[url('/big-sur.jpg')] bg-cover bg-center p-4 flex justify-center items-end"
     >
+      {contextMenu.show && (
+        <ContextMenu
+          x={contextMenu.x}
+          y={contextMenu.y}
+          closeContextMenu={contextMenuClose}
+        />
+      )}
       <Dock />
     </div>
   );
