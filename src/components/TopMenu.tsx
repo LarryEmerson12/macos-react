c035e8a6383d05bc194c92ed49de4843fe3d4feb
LarryEmerson12 <158800143+LarryEmerson12@users.noreply.github.com>
2025-01-21 16:36:33 +0800

    Use San Fransisco font

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -5,10 +5,10 @@ import React from "react";
 export default function TopMenu() {
   const menuItems = ["File", "Edit", "View", "Go", "Window", "Help"];
   return (
-    <div className="absolute flex w-full bg-white/35 h-6 backdrop-blur-lg top-0 p-1">
-      <span className="font-bold text-black text-xs mx-2 h-4">Finder</span>
+    <div className="absolute flex w-full bg-white/35 h-7 backdrop-blur-lg top-0 p-1">
+      <span className="font-bold text-black text-sm mx-2 h-4">Finder</span>
       {menuItems.map((i) => (
-        <span key={i} className="text-xs text-black mx-2 h-4">{i}</span>
+        <span key={i} className="text-sm text-black mx-2 h-4">{i}</span>
       ))}
     </div>
   );
