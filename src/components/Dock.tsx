0d05de103f91c74ce2f9871b54ac6d9a5402682f
LarryEmerson12 <158800143+LarryEmerson12@users.noreply.github.com>
2025-01-24 17:46:26 +0800

    Make animation better

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,44 +1,140 @@
-"use client";
+// Dependencies: npm i framer-motion tailwindcss @radix-ui/react-tooltip
 
-import React from "react";
-import Image from "next/image";
-import { motion } from "motion/react";
+'use client';
 
-type Images = {
-  [key: number]: string;
+import * as Tooltip from '@radix-ui/react-tooltip';
+import {
+  MotionValue,
+  animate,
+  motion,
+  useMotionValue,
+  useSpring,
+  useTransform,
+} from 'framer-motion';
+import { ReactNode, useRef } from 'react';
+
+const SCALE = 2.25; // max scale factor of an icon
+const DISTANCE = 110; // pixels before mouse affects an icon
+const NUDGE = 40; // pixels icons are moved away from mouse
+const SPRING = {
+  mass: 0.1,
+  stiffness: 170,
+  damping: 12,
 };
+const APPS = [
+  'Safari',
+  'Mail',
+  'Messages',
+  'Photos',
+  'Notes',
+  'Calendar',
+  'Reminders',
+  'Music',
+];
 
 export default function Dock() {
-  const images: Images = {
-    0: "finder",
-    1: "safari",
-    2: "xcode",
-    3: "terminal",
-    4: "apple-music",
-    5: "app-store",
-    6: "system-preferences",
-  };
+  const mouseLeft = useMotionValue(-Infinity);
+  const mouseRight = useMotionValue(-Infinity);
+  const left = useTransform(mouseLeft, [0, 40], [0, -40]);
+  const right = useTransform(mouseRight, [0, 40], [0, -40]);
+  const leftSpring = useSpring(left, SPRING);
+  const rightSpring = useSpring(right, SPRING);
 
   return (
-    <div className="min-w-[48ch] h-[7ch] bg-white/45 rounded-2xl flex justify-around items-center backdrop-blur-lg">
-      {Object.keys(images).map((key) => {
-        const index = Number(key);
-        return (
-          <React.Fragment key={index}>
-            <motion.div
-              whileHover={{ scale: 1.5 }}
-            >
-              <Image
-                src={`/${images[index]}.png`}
-                width={55}
-                height={55}
-                alt={images[index]}
-              />
-            </motion.div>
-            {index === 0 && <div className="h-8 w-px bg-gray-600 me-1"></div>}
-          </React.Fragment>
-        );
-      })}
-    </div>
+    <>
+      <motion.div
+        onMouseMove={(e) => {
+          const { left, right } = e.currentTarget.getBoundingClientRect();
+          const offsetLeft = e.clientX - left;
+          const offsetRight = right - e.clientX;
+          mouseLeft.set(offsetLeft);
+          mouseRight.set(offsetRight);
+        }}
+        onMouseLeave={() => {
+          mouseLeft.set(-Infinity);
+          mouseRight.set(-Infinity);
+        }}
+        className="mx-auto hidden h-16 items-end gap-3 px-2 pb-3 sm:flex relative"
+      >
+        <motion.div
+          className="absolute rounded-2xl inset-y-0 bg-gray-700 border border-gray-600 -z-10"
+          style={{ left: leftSpring, right: rightSpring }}
+        />
+
+        {Array.from(Array(APPS.length).keys()).map((i) => (
+          <AppIcon key={i} mouseLeft={mouseLeft}>
+            {APPS[i]}
+          </AppIcon>
+        ))}
+      </motion.div>
+    </>
+  );
+}
+
+function AppIcon({
+  mouseLeft,
+  children,
+}: {
+  mouseLeft: MotionValue;
+  children: ReactNode;
+}) {
+  const ref = useRef<HTMLButtonElement>(null);
+
+  const distance = useTransform(() => {
+    const bounds = ref.current
+      ? { x: ref.current.offsetLeft, width: ref.current.offsetWidth }
+      : { x: 0, width: 0 };
+
+    return mouseLeft.get() - bounds.x - bounds.width / 2;
+  });
+
+  const scale = useTransform(distance, [-DISTANCE, 0, DISTANCE], [1, SCALE, 1]);
+  const x = useTransform(() => {
+    const d = distance.get();
+    if (d === -Infinity) {
+      return 0;
+    } else if (d < -DISTANCE || d > DISTANCE) {
+      return Math.sign(d) * -1 * NUDGE;
+    } else {
+      return (-d / DISTANCE) * NUDGE * scale.get();
+    }
+  });
+
+  const scaleSpring = useSpring(scale, SPRING);
+  const xSpring = useSpring(x, SPRING);
+  const y = useMotionValue(0);
+
+  return (
+    <Tooltip.Provider delayDuration={0}>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <motion.button
+            ref={ref}
+            style={{ x: xSpring, scale: scaleSpring, y }}
+            onClick={() => {
+              animate(y, [0, -40, 0], {
+                repeat: 2,
+                ease: [
+                  [0, 0, 0.2, 1],
+                  [0.8, 0, 1, 1],
+                ],
+                duration: 0.7,
+              });
+            }}
+          >
+            Item
+          </motion.button>
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content
+            sideOffset={10}
+            className="z-index-99 bg-gray-700 shadow shadow-black border border-gray-600 px-2 py-1.5 text-sm rounded text-white font-medium"
+          >
+            {children}
+            <Tooltip.Arrow />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   );
 }
